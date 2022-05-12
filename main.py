118136a6f2579a0dfe6666d33014604464bd4135
Gabriel Bitti <gabriel.viana@ish.com.br>
2022-05-12 17:59:28 -0300

    Add try-except to treat console arguments

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 import socket
 import sys
 
-arguments_list = sys.argv
-arguments_list.pop(0)
-target = arguments_list[0]
-arguments_list.pop(0)
+try:
+    arguments_list = sys.argv
+    arguments_list.pop(0)
+    if len(arguments_list) == 0:
+        raise Exception('Missing host.')
 
-ports_to_scan = arguments_list
+    target = arguments_list[0]
+
+    if len(arguments_list) <= 1:
+        raise Exception('Missing ports number.')
+
+    ports_to_scan = arguments_list[1:]
+except Exception as e:
+    print(e)
+    exit(0)
+
 for port in ports_to_scan:
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.settimeout(1)
-    code = client.connect_ex((target, int(port)))
 
+    code = client.connect_ex((target, int(port)))
     if code == 0:
         print('Port {} OPEN'.format(port))
